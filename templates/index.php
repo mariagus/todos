b585b86e381c6d3ca4477736689958f02d050650
Maria Gusova <masha.gu@gmail.com>
2021-05-21 23:11:07 +0100

    add styling

--- templates/index.php
+++ templates/index.php
@@ -6,7 +6,9 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<header>
 <h1>Todos</h1>
+</header>
 <form method="post" action="/" class="input_form">
     <input type="text" name="task" class="task_input">
     <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
@@ -14,18 +16,20 @@
 <ul>
 <?php foreach($todos as $todo) {
     if ($todo['completed'] == 0) {
-        echo '<div class="container"><li>' . $todo['task'] .
-            '</li><a class="markDone" href="/markDone?id=' . $todo['id'] . '">️☑️</a>
+        echo '<div class="container"><a class="markDone" href="/markDone?id=' . $todo['id'] . '">️☑️</a>
+            <li>' . $todo['task'] . '</li>
             <a class="edit" href="/editTask?id=' . $todo['id'] . '&task=' . $todo['task'] . '">🖋</a>
             <a class="delete" href="/delete?id=' . $todo['id'] . '">🗑</a></div>';
     } else {
-        echo '<li>' . $todo['task'] .
-            '</li><a href="/delete?id=' . $todo['id'] . '">🗑</a>';
+        echo '<div class="container"><li class="completed">' . $todo['task'] .
+            '</li><a href="/delete?id=' . $todo['id'] . '">🗑</a></div>';
     }
 
 } ?>
 </ul>
+<div class="container">
 <a href="/">Uncompleted Todos</a>
 <a href="/completed">Completed Todos</a>
+</div>
 </body>
 </html>
